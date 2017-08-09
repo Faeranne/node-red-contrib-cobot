bb3acf14959c3df51294a1a120b3f5ce2afdcf5d
Blixa Morgan <blixa@projectmakeit.com>
2017-08-09 01:04:16 -0500

    change resource id field to fetch resource list from cobot

--- cobot-server.js
+++ cobot-server.js
@@ -1,3 +1,4 @@
+var request = require('superagent');
 module.exports = function(RED){
   function cobotServerNode(config) {
     RED.nodes.createNode(this,config);
@@ -5,5 +6,31 @@ module.exports = function(RED){
     this.subDomain = config.subDomain;
     this.token = config.token;
   }
+  RED.httpAdmin.get('/node-cobot/resources', function(req,res){
+    if(!req.query.server){
+      res.status(500).send("Missing arguments");
+      return;
+    }
+    var server = RED.nodes.getNode(req.query.server);
+    if(server && server.subDomain && server.token){
+      var URL = "https://"+server.subDomain+".cobot.me/api/resources"
+      request
+      .get(URL)
+      .set('Authorization', 'Bearer '+server.token)
+      .end(function(err, response){
+        if(err){
+          res.status(500).send("Internal Server Error: "+err);
+        }else{
+          var body = response.body;
+          try{
+            res.set({'content-type': 'application/json; charset=utf-8'})
+            res.end(JSON.stringify(body));
+          }catch (e){
+            res.status(500).send("Internal Server Error: "+e);
+          }
+        }
+      })
+    }
+  });	  
   RED.nodes.registerType("cobot-server", cobotServerNode);
 }
